bd08ecfa972453ed514cf0f49f7b637b2f43fa14
이재민 <cytor@naver.com>
2023-11-22 15:56:39 +0900

    fix: Fix kakaoMap

--- ssafy-vue(login_with_JWT)/src/views/TheTripView.vue
+++ ssafy-vue(login_with_JWT)/src/views/TheTripView.vue
@@ -38,9 +38,7 @@
         type="search"
         placeholder="검색어"
         aria-label="검색어" />
-      <button id="btn-search" class="btn btn-outline-success" type="button" @click="searchTrips">
-        검색
-      </button>
+      <button id="btn-search" class="btn btn-outline-success" type="button" @click="searchTrips">검색</button>
     </form>
     <!-- 관광지 검색 end -->
 
@@ -59,17 +57,12 @@
           </tr>
         </thead>
         <tbody id="trip-list">
-          <tr
-            v-for="(trip, tripIndex) in trips"
-            :key="trip.title"
-            @click="viewTrip(trip)">
+          <tr v-for="trip in trips" :key="trip.title" @click="viewTrip(trip)">
             <td><img :src="trip.firstimage" width="100" /></td>
             <td>{{ trip.title }}</td>
             <td>{{ trip.addr1 }} {{ trip.addr2 }}</td>
             <td>
-              <button class="btn btn-outline-secondary">
-                추가
-              </button>
+              <button class="btn btn-outline-secondary">추가</button>
             </td>
           </tr>
         </tbody>
@@ -81,14 +74,11 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import axios from "axios";
-import KakaoMap from "@/components/KakaoMap.vue";
 import VKakaoMap from "@/components/common/VKakaoMap.vue";
 // import kakao from "//dapi.kakao.com/v2/maps/sdk.js?appkey=63cd578ef6468c9ce78530d95cb5de9b&libraries=services,clusterer,drawing";
 const { VITE_OPEN_API_SERVICE_KEY } = import.meta.env;
 
 const baseUrl = "https://apis.data.go.kr/B551011/KorService1/";
-
-const selectedTrip = ref({});
 
 onMounted(() => {
   makeOption();
@@ -97,13 +87,14 @@ onMounted(() => {
 
 const trips = ref([]);
 const areas = ref([]);
+const selectedTrip = ref({});
 
-const viewTrip = (trip) => {
-  selectedTrip.value = trip;
-}
+const viewTrip = (select) => {
+  selectedTrip.value = select;
+};
 
-const getTripLists = () => {
-  axios
+const getTripLists = async () => {
+  const res = await axios
     .get(baseUrl + "areaBasedList1", {
       params: {
         serviceKey: VITE_OPEN_API_SERVICE_KEY,
@@ -114,17 +105,16 @@ const getTripLists = () => {
         _type: "json",
       },
     })
-    .then(({ data }) => {
-      trips.value = data.response.body.items.item;
-      console.log(data);
-    })
     .catch((err) => {
       console.log(err);
     });
+
+  trips.value = res.data.response?.body.items.item;
+  console.log(res);
 };
 
-const makeOption = () => {
-  axios
+const makeOption = async () => {
+  const res = await axios
     .get(baseUrl + "areaCode1", {
       params: {
         serviceKey: VITE_OPEN_API_SERVICE_KEY,
@@ -133,12 +123,10 @@ const makeOption = () => {
         _type: "json",
       },
     })
-    .then(({ data }) => {
-      areas.value = data.response.body.items.item;
-    })
     .catch((err) => {
       console.log(err);
     });
+  areas.value = res.data.response?.body.items.item;
 };
 
 // 검색
@@ -146,9 +134,9 @@ const searchArea = ref("0");
 const searchContentId = ref("0");
 const searchKeyword = ref([]);
 
-const searchTrips = () => {
+const searchTrips = async () => {
   if (searchKeyword.value == "") {
-    axios
+    const res = await axios
       .get(baseUrl + "areaBasedList1", {
         params: {
           serviceKey: VITE_OPEN_API_SERVICE_KEY,
@@ -159,14 +147,12 @@ const searchTrips = () => {
           _type: "json",
         },
       })
-      .then(({ data }) => {
-        trips.value = data.response.body.items.item;
-      })
       .catch((err) => {
         console.log(err);
       });
+    trips.value = res.data.response?.body.items.item;
   } else {
-    axios
+    const res = await axios
       .get(baseUrl + "searchKeyword1", {
         params: {
           serviceKey: VITE_OPEN_API_SERVICE_KEY,
@@ -178,91 +164,11 @@ const searchTrips = () => {
           _type: "json",
         },
       })
-      .then(({ data }) => {
-        trips.value = data.response.body.items.item;
-      })
       .catch((err) => {
         console.log(err);
       });
+    trips.value = res.data.response?.body.items.item;
   }
 };
-
-/*var positions; // marker 배열.
-function makeList(data) {
-  let markerInfo = {
-    title: area.title,
-    latlng: new kakao.maps.LatLng(area.mapy, area.mapx),
-    content: `<div class="card m-1" style="max-width: 540px;">
-                    <div class="row g-0">
-                      <div class="col-md-4">
-                        <img src=\${image} class="img-fluid rounded-start" alt="...">
-                      </div>
-                      <div class="col-md-8">
-                        <div class="card-body">
-                          <h5 class="card-title">\${area.title}</h5>
-                          <p class="card-text">\${area.addr1}</p>
-                          <p class="card-text"><small class="text-body-secondary">\${area.addr2}</small></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>`,
-    /!*`<div class=""><h style="vertical-align: middle">${area.title}</h><img src="${area.firstimage}" width="73" height="70"></div>`*!/
-  };
-  positions.push(markerInfo);
-  displayMarker();
-}
-
-function displayMarker() {
-  // 마커 이미지의 이미지 주소입니다
-  var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-  for (var i = 0; i < positions.length; i++) {
-    // 마커 이미지의 이미지 크기 입니다
-    var imageSize = new kakao.maps.Size(24, 35);
-
-    // 마커 이미지를 생성합니다
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-      map: map, // 마커를 표시할 지도
-      position: positions[i].latlng, // 마커를 표시할 위치
-      title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-      image: markerImage, // 마커 이미지
-    });
-    var infowindow = new kakao.maps.InfoWindow({
-      position: positions[i].latlng,
-      content: positions[i].content,
-    });
-
-    kakao.maps.event.addListener(marker, "mouseover", makeOverListener(map, marker, infowindow));
-    kakao.maps.event.addListener(marker, "mouseout", makeOutListener(infowindow));
-  }
-
-  // 첫번째 검색 정보를 이용하여 지도 중심을 이동 시킵니다
-  map.setCenter(positions[0].latlng);
-}
-
-function makeOverListener(map, marker, infowindow) {
-  return function () {
-    infowindow.open(map, marker);
-  };
-}
-
-// 인포윈도우를 닫는 클로저를 만드는 함수입니다
-function makeOutListener(infowindow) {
-  return function () {
-    infowindow.close();
-  };
-}
-
-function moveCenter(lat, lng) {
-  // map.setCenter(new kakao.maps.LatLng(lat, lng));
-}
-
-var pollContent = "";
-function addTravel(cnt) {
-  // pollContent += `<div>\${trips[cnt].title}  <button class="btn btn-outline-danger">삭제</button></div>`;
-  // document.querySelector("#poll-area").innerHTML = pollContent;
-}*/
 </script>
 <style></style>
